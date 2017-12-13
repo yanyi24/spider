33474a108ea97a189bcea7a190ec8ecd2ebe3106
yanyi_work <yanyi@9399@outlook.com>
2017-12-13 10:46:25 +0800

    init completed!

---
+++ app.js
@@ -0,0 +1,4 @@
+const path = require('path');
+const fs = require('fs');
+
+const config = require('./config/config');
---
+++ config/config.js
@@ -0,0 +1,6 @@
+const path = require('path');
+module.exports = {
+	netpath: 'https://www.multcloud.com/support-clouds/google-drive.html',
+	ismodifylocalfiles: false,
+	localpath: path.normalize('D:\\SVNWork\\web\\Websites\\MultCloud\\WebRoot\\WebRoot-New\\support-clouds\\google-drive.html')
+};
